fd5d6e7b01bd865b37ce20b1e1c1d0822d9e2e55
zyt6217315a <yingtao.zhu@foxmail.com>
2015-07-28 16:27:33 +0800

    add 1 new, 2015-07-28 11:22:23

---
+++ Test058.py
@@ -0,0 +1,16 @@
+__author__ = 'zyt'
+
+class Solution:
+    # @param {string} s
+    # @return {integer}
+    def lengthOfLastWord(self, s):
+        # if not s:
+        #     return 0
+        # try:
+        #     index = s.rindex(" ")
+        # except ValueError:
+        #     index = -1
+        # return len(s[index + 1:])
+        return len(s.strip().split(' ')[-1])
+
+print(Solution().lengthOfLastWord("    "))
