3e5875874ae9addbb7805b16b2d0f5f4cfa6d72b
zhuyingtao <yingtao.zhu@foxmail.com>
2015-10-29 17:00:30 +0800

    add nowcoder solutions

---
+++ Nowcoder/src/Offer5.java
@@ -0,0 +1,22 @@
+import java.util.Stack;
+
+/**
+ * Created by zyt on 15/10/28 14:46.
+ */
+public class Offer5 {
+    Stack<Integer> stack1 = new Stack<Integer>();
+    Stack<Integer> stack2 = new Stack<Integer>();
+
+    public void push(int node) {
+        stack1.push(node);
+    }
+
+    public int pop() {
+        if (stack2.isEmpty()) {
+            while (!stack1.isEmpty()) {
+                stack2.push(stack1.pop());
+            }
+        }
+        return stack2.pop();
+    }
+}
---
+++ Nowcoder/src/Offer6.java
@@ -0,0 +1,12 @@
+/**
+ * Created by zyt on 15/10/28 14:53.
+ */
+public class Offer6 {
+    public int minNumberInRotateArray(int[] array) {
+        int l = 0, h = array.length - 1;
+        while (l <= h) {
+            int m = (l + h) / 2;
+        }
+        return 0;
+    }
+}
