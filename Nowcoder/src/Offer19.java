0071f363475f15da601fd613f6ddfd3f1c1e1c67
zhuyingtao <yingtao.zhu@foxmail.com>
2015-12-21 16:14:09 +0800

    add Offer19

--- Nowcoder/src/Offer19.java
+++ Nowcoder/src/Offer19.java
@@ -6,9 +6,36 @@ import java.util.ArrayList;
 public class Offer19 {
     public ArrayList<Integer> printMatrix(int[][] matrix) {
         ArrayList<Integer> matrixList = new ArrayList<>();
-        for (int i = 0; i < matrix.length; i++) {
-
+        int count = 0;
+        int left = 0, right = matrix[0].length - 1;
+        int up = 0, bottom = matrix.length - 1;
+        while (count < matrix.length * matrix[0].length) {
+            for (int i = left; i <= right; i++, count++) {
+                matrixList.add(matrix[up][i]);
+            }
+            up++;
+            if (up > bottom) break;
+            for (int i = up; i <= bottom; i++, count++) {
+                matrixList.add(matrix[i][right]);
+            }
+            right--;
+            if (right < left) break;
+            for (int i = right; i >= left; i--, count++) {
+                matrixList.add(matrix[bottom][i]);
+            }
+            bottom--;
+            for (int i = bottom; i >= up; i--, count++) {
+                matrixList.add(matrix[i][left]);
+            }
+            left++;
         }
         return matrixList;
+    }
+
+    public static void main(String[] args) {
+        Offer19 offer19 = new Offer19();
+        //int[][] matrix = new int[][]{{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}, {13, 14, 15, 16}};
+        int[][] matrix = new int[][]{{1}, {2}, {3}, {4}, {5}};
+        System.out.println(offer19.printMatrix(matrix));
     }
 }
