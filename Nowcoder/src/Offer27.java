f0284ee3b0c77b7031dcf57f9513b7cf66276604
zhuyingtao <yingtao.zhu@foxmail.com>
2016-01-01 22:47:08 +0800

    just for signing =_=

---
+++ Nowcoder/src/Offer27.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+
+/**
+ * Created by zyt on 16/1/1 22:40.
+ */
+public class Offer27 {
+    public ArrayList<String> Permutation(String str) {
+        return null;
+    }
+}
