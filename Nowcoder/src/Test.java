bfbca217fe6d78decd683ff40e5d0949c5ed2dd4
zhuyingtao <yingtao.zhu@foxmail.com>
2016-01-13 16:17:31 +0800

    add Offer32

---
+++ Nowcoder/src/Test.java
@@ -0,0 +1,8 @@
+/**
+ * Created by zyt on 16/1/13 15:03.
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println(new Offer32().PrintMinNumber(new int[]{3, 5, 1, 4, 2}));
+    }
+}
